c94bc72e6efc5bf192412fe2553955b803c9a8cf
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 15:37:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let n1=prompt("Enter first number : ");
+let n2=prompt("Enter second number : ");
+
+n1=Number(n1);
+n2=Number(n2);
+alert(`The final result is: ${n1*2+n2}`);
